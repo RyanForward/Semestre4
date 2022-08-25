84bb2554d9e051df2e90f8d026f538e769af7abb
RyanEXT <83303260+RyanForward@users.noreply.github.com>
2022-08-24 23:17:24 -0300

    Adição de comentários

--- SIN110/Atividade 1/main.py
+++ SIN110/Atividade 1/main.py
@@ -7,4 +7,4 @@ matriz = entraDados(inst=sys.argv[1]) #envia como parametro a instância pré-de
 row, col = matriz  #informação do número de linhas e colunas são retiradas da tupla
 
 result = saiDados(inst=sys.argv[1], row=row, col=col) #envia para a função saiDados, disposta no arquivo create.py, os parametro de instância, número de linhas e número de colunas
-print(result) #Informa na tela o resultado com as dimensões da matriz
+print(result) #Informa na tela o resultado com as dimensões da matriz.
