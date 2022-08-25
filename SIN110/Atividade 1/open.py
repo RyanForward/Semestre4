c0be4534e943ec9a569a28e0a2d2eba1799fcdef
RyanEXT <83303260+RyanForward@users.noreply.github.com>
2022-08-24 21:07:16 -0300

    Adição de comentários

--- SIN110/Atividade 1/open.py
+++ SIN110/Atividade 1/open.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 
 def entraDados(inst):
-    path = os.path.join(os.getcwd() + '\\DATASET_GRAFOS\\' + inst + '.txt')
-    matriz = np.loadtxt(path, dtype=int)
-    return matriz.shape
 
+    path = os.path.join(os.getcwd() + '\\DATASET_GRAFOS\\' + inst + '.txt') # abre a instância informada como parâmetro
+    matriz = np.loadtxt(path, dtype=int) # carrega do arquivo .txt a matriz já a transformando para o formato NumPy
+    return matriz.shape # retorna as dimensões da matriz
