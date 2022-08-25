853398d367d4edffe606e8fa1009ee261ee6d7ea
RyanEXT <83303260+RyanForward@users.noreply.github.com>
2022-08-24 22:25:01 -0300

    Adicionando comentários

--- SIN110/Atividade 1/main.py
+++ SIN110/Atividade 1/main.py
@@ -1,11 +1,10 @@
 from open import entraDados
 from create import saiDados
+import sys  #sistema
 
-import sys
+matriz = entraDados(inst=sys.argv[1]) #envia como parametro a instância pré-definida pelo usuário
 
-matriz = entraDados(inst=sys.argv[1])
+row, col = matriz  #informação do número de linhas e colunas são retiradas da tupla
 
-row, col = matriz
-
-result = saiDados(inst=sys.argv[1], row=row, col=col)
-print(result)
+result = saiDados(inst=sys.argv[1], row=row, col=col) #envia para a função saiDados, disposta no arquivo create.py, os parametro de instância, número de linhas e número de colunas
+print(result) #Informa na tela o resultado com as dimensões da matriz
