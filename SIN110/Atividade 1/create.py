3af305358fb87373698990ea34c63809bcae286a
RyanEXT <83303260+RyanForward@users.noreply.github.com>
2022-08-24 22:46:26 -0300

    Adicionando comentários

--- SIN110/Atividade 1/create.py
+++ SIN110/Atividade 1/create.py
@@ -1,8 +1,8 @@
 def saiDados(inst, row, col):
-    nome = 'Resultado.txt'
-    file = open(nome, 'w+')
-    text = str(inst + ' ' + str(row) + ' ' + str(col))
-    file.write(text)
-    file.close()
+    nome = 'Resultado.txt'  # Nome do arquivo
+    file = open(nome, 'w+') # Abre com a permissão de escrita
+    text = str(inst + ' ' + str(row) + ' ' + str(col)) # Gera o texto com as informações a serem escritas
+    file.write(text) # Salva no arquivo txt
+    file.close() # Fecha o arquivp
 
-    return text
+    return text # Retorna o texto formatado para impressão na tela
